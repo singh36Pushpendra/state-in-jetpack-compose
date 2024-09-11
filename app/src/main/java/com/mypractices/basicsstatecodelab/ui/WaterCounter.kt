e9722642a29bcd1d6962819b2d51db199e8d6841
Pushpendra Singh Thakur <pushpendra.singh@myn.global>
2024-09-11 16:08:58 +0530

    first commit

---
+++ app/src/main/java/com/mypractices/basicsstatecodelab/ui/WaterCounter.kt
@@ -0,0 +1,62 @@
+package com.mypractices.basicsstatecodelab.ui;
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun WaterCounter(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.padding(16.dp)
+    ) {
+        // Changes are now tracked by Compose
+        var count by remember { mutableStateOf(0) }
+
+        if (count > 0) {
+            var showTask by remember {
+                mutableStateOf(true)
+            }
+            if (showTask) {
+                WellnessTaskItem(
+                    taskName = "Have you taken your 15 minute walk today?",
+                    onClose = { showTask = false })
+            }
+            // This text is present if the button has been clicked.
+            // at least once; otherwise absent
+            Text(
+                text = "You've had ${count} glasses."
+            )
+        }
+        Row(
+            modifier = Modifier.padding(top = 8.dp)
+        ) {
+            Button(
+                onClick = { count++ },
+                enabled = count < 10
+            ) {
+                Text(text = "Add one")
+            }
+            Button(
+                onClick = { count = 0 }, Modifier.padding(start = 8.dp)
+            ) {
+                Text(text = "Clear water count")
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun WaterCounterPreview() {
+    WaterCounter()
+}
