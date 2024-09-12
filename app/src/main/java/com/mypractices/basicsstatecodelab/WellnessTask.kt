52956ccda0aec129fe796fa6b44ddccaac2f21f1
Pushpendra Singh Thakur <pushpendra.singh@myn.global>
2024-09-12 14:56:11 +0530

    work with lists

---
+++ app/src/main/java/com/mypractices/basicsstatecodelab/WellnessTask.kt
@@ -0,0 +1,6 @@
+package com.mypractices.basicsstatecodelab
+
+data class WellnessTask(
+    val id: Int,
+    val label: String
+)
--- app/src/main/java/com/mypractices/basicsstatecodelab/ui/WellnessScreen.kt
+++ app/src/main/java/com/mypractices/basicsstatecodelab/ui/WellnessScreen.kt
@@ -1,9 +1,14 @@
 package com.mypractices.basicsstatecodelab.ui
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.mypractices.basicsstatecodelab.WellnessTasksList
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
-    StatefulCounter(modifier)
+    Column(modifier = modifier) {
+        StatefulCounter()
+        WellnessTasksList()
+    }
 }
